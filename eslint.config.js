880dadb045ebaa4d551af49b65afc4338c72a047
Miroslav Bajtoš <oss@bajtos.net>
2025-04-10 16:01:46 +0200

    remove config specific to CheckerNetwork/node
    - env
    - ignores
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,4 @@ import neostandard from 'neostandard'
 export default neostandard({
   noStyle: true, // Disable style-related rules, we use Prettier
   ts: true,
-  env: ['mocha'],
-  ignores: ['.cache/**', 'test/.cache/**'],
 })
